e4425119616ecb2aecd95268ec94d883d9378c99
Edoardo <iweek16@gmail.com>
2019-02-27 12:00:25 +0100

    Update dependecies

--- app/src/main/java/com/civic/gercs/civicsense/EndPoint.java
+++ app/src/main/java/com/civic/gercs/civicsense/EndPoint.java
@@ -4,7 +4,7 @@ public class EndPoint {
 
 
 
-    public final static String END_POINT_LOCAL      = "http://192.168.1.181/CivicSensWeb/";
+    public final static String END_POINT_LOCAL      = "http://192.168.0.100/CivicSensWeb/";
     public final static String END_POINT_ALTERVISTA = "https://civicsens.altervista.org/";
 
 
